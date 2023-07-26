40c476f57ae9d8cad57de89695d3a8ea93f91911
unknown <niteshmodi00007@gmail.com>
2023-07-26 20:10:24 +0530

    Todo List by webdev

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect, useState } from "react"
 import './App.css';
+import NewTodoForm from "./Components/NewTodoForm";
+import Todolist from "./Components/Todolist";
 
 function App() {
+  const [todos, setTodos] = useState(() =>{
+    const localValue = localStorage.getItem("ITEMS")
+    if(localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title){
+    setTodos((currentTodos)=>{
+      return [
+        ...currentTodos, { id: crypto.randomUUID(), 
+        title, completed: false},
+      ]
+    })
+  }
+  
+  var toggleTodo = (id,completed) => {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id === id){
+          return { ...todo, completed }
+        }
+        return todo;
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  
+  // Instead of returning a div so that the full jsx 
+  // could come under one html tag
+  // we could avoid the extra div by using fragment <></>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>  
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <Todolist 
+        todos={todos} 
+        toggleTodo={toggleTodo} 
+        deleteTodo={deleteTodo} />
+    </>
   );
 }
 
